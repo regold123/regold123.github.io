a71de19e9c25ca343cd4c22e672579ae62481b44
regold123 <ari@replyall.me>
2020-06-14 08:10:33 -0400

    Updates

---
+++ precache-manifest.b5933020d7fb675531fc8cc618aa237c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fdce594c2f28aed5bdbdd047c83285",
+    "url": "/playbookquiz/index.html"
+  },
+  {
+    "revision": "e20fe572a7db99ed5368",
+    "url": "/playbookquiz/static/css/main.ab92684e.chunk.css"
+  },
+  {
+    "revision": "bac366603a4953b30146",
+    "url": "/playbookquiz/static/js/2.2b3d42ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/playbookquiz/static/js/2.2b3d42ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20fe572a7db99ed5368",
+    "url": "/playbookquiz/static/js/main.e52ad89c.chunk.js"
+  },
+  {
+    "revision": "533f18968a53d9485931",
+    "url": "/playbookquiz/static/js/runtime-main.cde9dfdf.js"
+  },
+  {
+    "revision": "2d8225217aa2ab1159167c917130e884",
+    "url": "/playbookquiz/static/media/90G.2d822521.jpg"
+  },
+  {
+    "revision": "8228bde631db16d3b87990c60f757f8e",
+    "url": "/playbookquiz/static/media/90L.8228bde6.jpg"
+  },
+  {
+    "revision": "67feb7b6c5cb29baa4b7a243a4db1bbf",
+    "url": "/playbookquiz/static/media/90N.67feb7b6.jpg"
+  },
+  {
+    "revision": "8419956abb60e9e44a7c2707695e6359",
+    "url": "/playbookquiz/static/media/90R.8419956a.jpg"
+  },
+  {
+    "revision": "44b68f62e04e153f36d61ccc4db08dd1",
+    "url": "/playbookquiz/static/media/91G.44b68f62.jpg"
+  },
+  {
+    "revision": "f02cf9aac0830c8ed3082ecd058ea9e0",
+    "url": "/playbookquiz/static/media/91L.f02cf9aa.jpg"
+  },
+  {
+    "revision": "6049a43b940def471e4cd0d2686bf4e3",
+    "url": "/playbookquiz/static/media/91N.6049a43b.jpg"
+  },
+  {
+    "revision": "a0a6a0f516dcee3ab602a8f8a52d8527",
+    "url": "/playbookquiz/static/media/91R.a0a6a0f5.jpg"
+  },
+  {
+    "revision": "5bdb4c1892d06d0bfcc3b7afeb423329",
+    "url": "/playbookquiz/static/media/93G.5bdb4c18.jpg"
+  },
+  {
+    "revision": "a314e85f14085e5524d5323616507631",
+    "url": "/playbookquiz/static/media/93L.a314e85f.jpg"
+  },
+  {
+    "revision": "da34a5ae39d792cf5037483b6152247b",
+    "url": "/playbookquiz/static/media/93N.da34a5ae.jpg"
+  },
+  {
+    "revision": "efd8e048eda7701ba825e995acdb1326",
+    "url": "/playbookquiz/static/media/93R.efd8e048.jpg"
+  },
+  {
+    "revision": "5cc6e966f1fad9df4bb6f33ffe5ffd72",
+    "url": "/playbookquiz/static/media/96G.5cc6e966.jpg"
+  },
+  {
+    "revision": "4afeaeb7fc859a33c4e8f74651688576",
+    "url": "/playbookquiz/static/media/96L.4afeaeb7.jpg"
+  },
+  {
+    "revision": "9b204301f3ab310a89000841732e86f7",
+    "url": "/playbookquiz/static/media/96N.9b204301.jpg"
+  },
+  {
+    "revision": "a397f34064f8a0cc3ce698b7383c9837",
+    "url": "/playbookquiz/static/media/96R.a397f340.jpg"
+  }
+]);
